83a8c1e1075e21d111c3a400a1cf6be35934bd88
Roky Perez <yassper0708@gmail.com>
2022-08-09 17:32:48 -0500

    Primera version Sass

---
+++ scss/scrips/_menu.js
@@ -0,0 +1,10 @@
+const toggle = document.querySelector('.navegar__btn');
+const menu = document.querySelector('.navegar__menu');
+
+function toggleMenu(){
+
+}
+
+toggle.addEventListener('click',toggleMenu, false);
+
+
